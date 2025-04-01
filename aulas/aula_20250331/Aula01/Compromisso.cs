795cb6ce19d53a77547902837fc5bb76450f32c8
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-03-31 22:35:27 -0300

    Final Aula 01

---
+++ aulas/aula_20250331/Aula01/Compromisso.cs
@@ -0,0 +1,7 @@
+class Compromisso
+{
+    public string Data { get; set; }
+    public string Hora { get; set; }
+    public string Local { get; set; }
+    public string Descricao { get; set; }
+}
---
+++ aulas/aula_20250331/Aula01/Program.cs
@@ -0,0 +1,20 @@
+﻿// namespace Aula01 {
+//     class Program {
+//         public static void Main(String[] args) {
+//             Console.WriteLine("Olá UTFPR");
+//         }
+//     }
+// }
+
+// // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá UTFPR");
+
+// List<Compromisso> agenda = new List<Compromisso>();
+
+// agenda.Add(new Compromisso());
+
+// Console.WriteLine(agenda.ElementAt(0));
+
+// agenda[0] = new Compromisso();
+
+// agenda.RemoveAt(0);
